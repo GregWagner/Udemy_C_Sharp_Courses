b4ca99fe34c79cb4ecbc33083f30bd2cb73eca67
GregWagner <theGreg101@gmail.com>
2024-09-13 08:04:03 -0500

    Initial todolist app

---
+++ Ultimate_C_Sharp_Masterclass/Section_02_Fundamentals/TodoList/TodoList/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+var userChoice = Console.ReadLine();
+Console.WriteLine(userChoice);
+userChoice = "ABCD";
+Console.WriteLine(userChoice);
+
+Console.ReadKey();
